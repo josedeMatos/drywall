f4fcff99eec45d382455fbc984ac16fa232294e4
josedematos <jose@swogo.com>
2015-12-15 17:56:33 +0000

    settings update

--- views/account/index.js
+++ views/account/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
-exports.init = function(req, res){
-  res.render('account/index');
+exports.init = function(req, res,next){
+  var account=(req.user.roles.account);
+  console.log(account);
+  res.render('account/index',account);
 };
