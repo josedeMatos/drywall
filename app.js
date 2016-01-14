427a70a8db1ad69e4794ebddd6971a68f708a2ac
josedematos <jose@swogo.com>
2016-01-14 14:25:30 +0000

    clean up start

--- app.js
+++ app.js
@@ -59,16 +59,20 @@ app.use(csrf({ cookie: { signed: true } }));
 helmet(app);
 
 //CORS permissions
+/*
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
+*/
 
 //response locals
 app.use(function(req, res, next) {
-  res.cookie('_csrfToken', req.csrfToken());
+  var cookietemp=req.csrfToken();
+  console.log("--DEBUG@ cookie crsf"+cookietemp);
+  res.cookie('_csrfToken', cookietemp);
   res.locals.user = {};
   res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
   res.locals.user.username = req.user && req.user.username;
